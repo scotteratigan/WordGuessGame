16156139b97a3d63e125c15d9c5369a582b51d8b
Scott Ratigan <scotteratigan@gmail.com>
2018-10-06 16:04:16 -0700

    Refactored a bit.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,105 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var phrasesToGuess = ["gastropub", "farm to table", "edison bulb", "homemade bitters", "ironic tattoo", "eight bits", "single origin coffee", "ironic prose", "taxidermied fish", "cornhole tournament", "pabst blue ribbon", "homemade kombucha", "succulent garden", "beard fashion", "vegan beef", "free range veggies", "neon fanny pack"];
 var computerSecretPhrase = "";
-var userGuessedLetters = [];
+var guessedLetterList = [];
 var wrongGuessCount;
 const maxWrongGuesses = 10;
 var userGuessDisplay;
-// todo: add a gameInProgress var - don't process keyboard input if game is not in progress...
-// var startGameButton = document.getElementById("start-hangman-game");
+var gameInProgress = false;
+
 // todo: hash the phrases so that the TAs can't cheat!
 
 function displayRemainingLetters() {
 	var remainingLetterBox = document.getElementById("remaining-letters");
 	var displayString = "";
 	for(var i = 0; i < alphabet.length; i++) {
-		displayString += '<span class="hangman-letter">';
-		//displayString += "<p>";
-		if (!userGuessedLetters.includes(alphabet[i])) {
+		if (!guessedLetterList.includes(alphabet[i])) {
 			displayString += alphabet[i];
 		}
 		else {
 			displayString += "_";
 		}
-		displayString += "</span>";
-		//displayString += "</p>";
 	}
-	//remainingLetterBox.innerHTML = "<p>" + displayString + "</p>";
-	remainingLetterBox.innerHTML = displayString;
+	remainingLetterBox.innerHTML = "<p>" + displayString + "</p>";
 }
 
 function displayGuessArea() {
 	var guessArea = document.getElementById("guess-area");
 	userGuessDisplay = "";
 	for(var i = 0; i < computerSecretPhrase.length; i++) {
-		userGuessDisplay += '<span class="hangman-letter">';
-		if (userGuessedLetters.includes(computerSecretPhrase[i])) {
+		if (guessedLetterList.includes(computerSecretPhrase[i])) {
 			userGuessDisplay += computerSecretPhrase[i];
-			//userGuessDisplay += " "
-		} // todo: see if two statements can be combined above
+		}
 		else if (computerSecretPhrase[i] == " ") {
-			userGuessDisplay += "-"; // todo: add span with class id rather than spaces
+			userGuessDisplay += "-";
 		}
 		else {
 			userGuessDisplay += "_";
 		}
-		userGuessDisplay += "</span>";
 	}
 	console.log("userGuessDisplay is: " + userGuessDisplay);
 	guessArea.innerHTML = "<p>" + userGuessDisplay + "</p>";
 }
 
+function displayGuessesRemaining() {
+	var guessesRemainingArea = document.getElementById("guesses-remaining");
+	var wrongGuessesRemaining = maxWrongGuesses - wrongGuessCount;
+	guessesRemainingArea.innerHTML = "<p>Wrong Guesses Remaining: " + wrongGuessesRemaining;
+}
+
 function startHangmanGame() {
+	gameInProgress = true;
 	wrongGuessCount = 0;
 	computerSecretPhrase = phrasesToGuess[Math.floor(Math.random() * phrasesToGuess.length)];
-	userGuessedLetters = [];
+	guessedLetterList = [];
 	displayGuessArea();
 	displayRemainingLetters();
+	displayGuessesRemaining();
 }
 
 document.getElementById("start-hangman-game").onclick = startHangmanGame;
 
-document.onkeyup = function(event) {
-	var userKey = event.key;
-	//if (userKey < 'a' || userKey > 'z') {
-	//	return;
-	//}
-	if (userKey.toLowerCase() == userKey.toUpperCase) {
-		return;
-	}
-	if (userGuessedLetters.includes(userKey)) {
+// currently refactoring the function below
+// also, adding in the boolean gameInProgress (did I finish that?)
+// next, add scores
+// then add modal for win/loss
+// then add style in general
+
+function userGuessesLetter(userKey) {
+	if (guessedLetterList.includes(userKey)) {
 		alert("You already guessed " + userKey + "!");
 		return;
 	}
-	else { // user guessed a new letter, add to guessed list and check for win or loss
-		userGuessedLetters.push(userKey);
+	//displayGuessesRemaining();
+	//else { // user guessed a new letter, add to guessed list and check for win or loss
+		displayGuessesRemaining();
+		guessedLetterList.push(userKey);
 		displayGuessArea();
 		displayRemainingLetters();
 		
 		if (!userGuessDisplay.includes('_')) { // if there are no underscores left in display, user has won:
 			setTimeout(function() { 
 				alert("YOU WON BRO");
-			}, 700);
+			}, 100);
 		}
 		if (!computerSecretPhrase.includes(userKey)) { // Bad Guess
 			wrongGuessCount++;
 			if (wrongGuessCount > maxWrongGuesses) {
 				setTimeout(function() { 
 					alert("YOU LOST BRO");
-				}, 700);
+				}, 100);
 			}
 		}
-	}
+	//}
 }
 
-//startGameButton.addEventListener("click", startHangmanGame()); <- caused immediate execution beause of (), are you kidding me?!
-//startGameButton.addEventListener("click", startHangmanGame; <- correct syntax
+document.onkeyup = function(event) {
+	//var userKey = event.key;
+	if (!gameInProgress) {
+		return
+	}
+	if (event.key < 'a' || event.key > 'z') {
+		return;
+	}
+	userGuessesLetter(event.key);
+}
